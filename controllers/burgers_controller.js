e3057fed752302fd7ac4b71ec0470b852461c7b0
Sanjula <sanjula8@gmail.com>
2020-02-28 02:46:45 -0600

    added routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,16 @@
 //    * `burger.js`
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
+
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", function(req, res) {});
+
+router.post("/", function(req, res) {});
+
+router.put("/:id", function(req, res) {});
+
+module.exports = router;
