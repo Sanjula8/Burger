b7e84bcdc475d9dc7a18d97e151b6eabc68b6dce
Sanjula <sanjula8@gmail.com>
2020-02-27 22:56:10 -0600

    added seeds and schema :

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,11 @@
--- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burgerName) VALUES
+("Gourdon-Hamsey Burger (comes with squash and ham)"),
+("Poutine on the Ritz Burger (comes with poutine fries)"),
+("Eggers Can't Be Cheesers (with fried egg and cheese)"),
+("Foot Feta-ish Burger"),
+("Olive and Let Die Burger"),
+("Hit me with Your Best Shallot Burger"),
+("Cheeses is Born Burger (comes with baby Swiss cheese)"),
+("Don't You Four Cheddar 'Bout Me Burger (comes with four kinds of cheddar)"),
+("Chorizo Your Own Adventure Burger"),
+("Beets Me Burger")
