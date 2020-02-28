953d7f7354618f16ce7bc51f27fab56de812acd0
Sanjula <sanjula8@gmail.com>
2020-02-28 00:33:39 -0600

    added require handlebars and routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // 7. Require the following npm packages inside of the server.js file:
 //    * express
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller");
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -14,13 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
